65c97d2a3481e87332d60479ff3ecbfc08f11eee
Mohd Bilal <98958767+mohd-Bilal-exe@users.noreply.github.com>
2024-08-07 18:57:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   const [darkMode, setDarkMode] = useState(false);
   return (
-    <section className={`w-screen h-full ${darkMode ? "bg-background" : "bg-backgroundLight"} transition-colors overflow-x-hidden overflow-y-auto`}>
+    <section className={`w-screen h-screen ${darkMode ? "bg-background" : "bg-backgroundLight"} transition-colors overflow-x-hidden overflow-y-auto`}>
       <Navbar setDarkMode={setDarkMode} darkMode={darkMode} />
       <HomePage darkMode={darkMode} />
     </section>
